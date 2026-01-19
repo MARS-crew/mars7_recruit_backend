c06bb436c7abfc341fe0d10b23ce17749daecced
chldPDms <115327796+chldPDms@users.noreply.github.com>
2026-01-19 16:40:50 +0900

    배포 에러 해결...

--- src/main/java/com/mars7/mars7_recruit_backend/common/config/auth/config/SecurityConfig.java
+++ src/main/java/com/mars7/mars7_recruit_backend/common/config/auth/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
